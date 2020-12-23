4151021d155a7b25c954d97d78a5ea7aceb0d598
Debbie Cheng <dqrcheng@gmail.com>
2020-12-23 15:07:54 +1100

    Updates

---
+++ precache-manifest.7341863118ddd8f8f995943629552f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cc29577becb5bb005b7ab316737836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c791d08d5b34bdc88a54",
+    "url": "/static/css/main.237fb503.chunk.css"
+  },
+  {
+    "revision": "65f1516d73885b0ac14e",
+    "url": "/static/js/2.4a767b03.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4a767b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c791d08d5b34bdc88a54",
+    "url": "/static/js/main.cf1cd8a8.chunk.js"
+  },
+  {
+    "revision": "1b68c7ec44968e123a64",
+    "url": "/static/js/runtime-main.95dbbc22.js"
+  }
+]);
